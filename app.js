6c07344134d65212e70f518a81872d1459ca8777
Bruno Verdan <bruno.verdan@al.infnet.edu.br>
2020-10-15 17:21:13 -0300

    Chamando as informações do banco

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const app = express();
 const db = require('./db/connection');
 const bodyParser = require('body-parser');
 const path = require('path');
+const Job = require('./models/Job');
+
 
 const PORT = 3000;
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log(`O express está rodando na porta ${PORT}`);
 });
 
 //body parser
 app.use(bodyParser.urlencoded({ extended: false }));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 //handle bars
@@ -25,14 +27,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 //db connection
 db.authenticate()
     .then(() => {
-    console.log("Conectou ao banco com sucesso");
-}).catch(err => {
-    console.log("Ocorreu um erro ao conectar", err);
-});
+        console.log("Conectou ao banco com sucesso");
+    }).catch(err => {
+        console.log("Ocorreu um erro ao conectar", err);
+    });
 
 //routes
 app.get("/", (req, res) => {
-    res.render('index');
+    Job.findAll({
+        order: [
+            ['createdAt', 'DESC']
+        ]
+    })
+    .then(jobs => {
+        res.render('index', {
+            jobs
+        });
+    });
 });
 
 //jobs routes
